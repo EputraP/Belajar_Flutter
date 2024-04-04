54ee900e815f45f2ede37c7553362fce70a4a44d
eputrap <eputrap@gmail.com>
2024-04-04 21:33:09 +0700

    20240404_Ev

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,43 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("Hello World"),
+        backgroundColor: const Color.fromARGB(255, 155, 144, 165),
+        body: Container(
+          alignment: Alignment.center,
+          child: GradientContainer(),
         ),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      width: MediaQuery.sizeOf(context).width * 0.5,
+      height: MediaQuery.sizeOf(context).height * 0.5,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 90)
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello World",
+          style: TextStyle(
+            color: Colors.white70,
+            fontSize: 28.0,
+          ),
+        ),
+      ),
+    );
+  }
 }
