e86ead25bb41ee9c23b381b82f8dc5223bb40eab
eputrap <eputrap@gmail.com>
2024-04-30 06:52:23 +0700

    20240430_morning

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      backgroundColor: Color.fromARGB(100, 72, 0, 144),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("image", style: TextStyle(color: Colors.white54)),
+          Text("APp"),
+          Text("button"),
+        ],
+      ),
+    ),
+  ));
+}
