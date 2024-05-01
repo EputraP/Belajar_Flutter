40b6581c0e2271e37195fb42b35c7e113707cb7c
eputrap <eputrap@gmail.com>
2024-05-02 06:06:31 +0700

    20240502

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      backgroundColor: Color.fromARGB(100, 72, 0, 144),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text("image", style: TextStyle(color: Colors.white54)),
-          Text("APp"),
-          Text("button"),
-        ],
-      ),
-    ),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Color.fromARGB(100, 72, 0, 144),
+          body: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/quiz-logo.png',
+                  width: 280,
+                ),
+                const SizedBox(
+                  height: 70,
+                ),
+                const Text("Learn Flutter the fun way!",
+                    style: TextStyle(color: Colors.white, fontSize: 24)),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Start Quiz",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
+  }
 }
